2893ccda241b2ba8d474b0ba9ce4e56b6db9ba10
$udiSimba <sudisimbadav@gmail.com>
2020-04-11 14:30:46 +0200

    Modified js file

--- js/app.js
+++ js/app.js
@@ -1,83 +1,28 @@
-const container = document.querySelector(".container");
-var inputValue = document.querySelector(".input");
-const add = document.querySelector(".add");
+var btn = document.getElementById("add");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
-if (window.localStorage.getItem("todos") == undefined) {
-  var todos = [];
-  window.localStorage.setItem("todos", JSON.stringify(todos));
+function inputLength() {
+  return input.value.length;
 }
 
-var todosEX = window.localStorage.getItem("todos");
-var todos = JSON.parse(todosEX);
-
-class item {
-  constructor(name) {
-    this.createItem(name);
+function createEventElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+function AddListAfterClick() {
+  if (inputLength() > 0) {
+    createEventElement();
   }
-  createItem(name) {
-    var itemBox = document.createElement("div");
-    itemBox.classList.add("item");
-
-    var input = document.createElement("input");
-    input.type = "text";
-    input.disabled = true;
-    input.value = name;
-    input.classList.add("item_input");
-
-    var edit = document.createElement("button");
-    edit.classList.add("edit");
-    edit.innerHTML = "EDIT";
-    edit.addEventListener("click", () => this.edit(input, name));
-
-    var remove = document.createElement("button");
-    remove.classList.add("remove");
-    remove.innerHTML = "REMOVE";
-    remove.addEventListener("click", () => this.remove(itemBox, name));
-
-    container.appendChild(itemBox);
-
-    itemBox.appendChild(input);
-    itemBox.appendChild(edit);
-    itemBox.appendChild(remove);
-  }
-
-  edit(input, name) {
-    if (input.disabled == true) {
-      input.disabled = !input.disabled;
-    } else {
-      input.disabled = !input.disabled;
-      let indexof = todos.indexOf(name);
-      todos[indexof] = input.value;
-      window.localStorage.setItem("todos", JSON.stringify(todos));
-    }
-  }
-
-  remove(itemBox, name) {
-    itemBox.parentNode.removeChild(itemBox);
-    let index = todos.indexOf(name);
-    todos.splice(index, 1);
-    window.localStorage.setItem("todos", JSON.stringify(todos));
+}
+function AddListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createEventElement();
   }
 }
 
-add.addEventListener("click", check);
-window.addEventListener("keydown", (e) => {
-  if (e.which == 13) {
-    check();
-  }
-});
+btn.addEventListener("click", AddListAfterClick);
 
-function check() {
-  if (inputValue.value != "") {
-    new item(inputValue.value);
-    todos.push(inputValue.value);
-    window.localStorage.setItem("todos", JSON.stringify(todos));
-    inputValue.value = "";
-  }
-}
-
-for (var v = 0; v < todos.length; v++) {
-  new item(todos[v]);
-}
-
-new item("sport");
+input.addEventListener("keypress", AddListAfterKeypress);
